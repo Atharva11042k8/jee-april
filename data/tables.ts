f364ed06e2715af5520dfeda739419b158f4844d
Atharva <169069337+Atharva11042k8@users.noreply.github.com>
2026-01-31 11:58:15 +0530

    Update tables.ts

--- data/tables.ts
+++ data/tables.ts
@@ -21,7 +21,7 @@ export const tables = [
         id: "r1",
         data: {
           c1: "Units & Measurements (UOM)",
-          c2: "Not Started",
+          c2: "",
           c3: { sc1: "", sc2: "", sc3: "" }
         },
         notes: ""
@@ -30,7 +30,7 @@ export const tables = [
         id: "r2",
         data: {
           c1: "Vectors",
-          c2: "Not Started",
+          c2: "",
           c3: { sc1: "", sc2: "", sc3: "" }
         },
         notes: ""
@@ -39,7 +39,7 @@ export const tables = [
         id: "r3",
         data: {
           c1: "Kinematics",
-          c2: "Not Started",
+          c2: "",
           c3: { sc1: "", sc2: "", sc3: "" }
         },
         notes: ""
@@ -48,7 +48,7 @@ export const tables = [
         id: "r4",
         data: {
           c1: "Newton’s Laws of Motion (NLM)",
-          c2: "Not Started",
+          c2: "",
           c3: { sc1: "", sc2: "", sc3: "" }
         },
         notes: ""
@@ -66,7 +66,7 @@ export const tables = [
         id: "r6",
         data: {
           c1: "Work, Power & Energy (WPE)",
-          c2: "Not Started",
+          c2: "",
           c3: { sc1: "", sc2: "", sc3: "" }
         },
         notes: ""
@@ -75,7 +75,7 @@ export const tables = [
         id: "r7",
         data: {
           c1: "Centre of Mass (COM)",
-          c2: "Not Started",
+          c2: "",
           c3: { sc1: "", sc2: "", sc3: "" }
         },
         notes: ""
@@ -92,87 +92,87 @@ export const tables = [
 
       {
         id: "r9",
-        data: { c1: "Gravitation", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Gravitation", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r10",
-        data: { c1: "Heat & Thermodynamics", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Heat & Thermodynamics", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r11",
-        data: { c1: "Properties of Matter", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Properties of Matter", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r12",
-        data: { c1: "Electrostatics", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Electrostatics", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r13",
-        data: { c1: "Capacitors", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Capacitors", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r14",
-        data: { c1: "Current Electricity", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Current Electricity", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r15",
-        data: { c1: "Magnetic Effect of Current", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Magnetic Effect of Current", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r16",
-        data: { c1: "Magnetic Properties of Matter", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Magnetic Properties of Matter", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r17",
-        data: { c1: "Electromagnetic Induction (EMI)", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Electromagnetic Induction (EMI)", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r18",
-        data: { c1: "Alternating Current (AC)", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Alternating Current (AC)", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r19",
-        data: { c1: "Semiconductors", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Semiconductors", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r20",
-        data: { c1: "Modern Physics", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Modern Physics", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r21",
-        data: { c1: "Simple Harmonic Motion (SHM)", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Simple Harmonic Motion (SHM)", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r22",
-        data: { c1: "Waves", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Waves", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r23",
-        data: { c1: "Electromagnetic Waves", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Electromagnetic Waves", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r24",
-        data: { c1: "Wave Optics", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Wave Optics", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       },
       {
         id: "r25",
-        data: { c1: "Ray Optics", c2: "Not Started", c3: { sc1: "", sc2: "", sc3: "" } },
+        data: { c1: "Ray Optics", c2: "", c3: { sc1: "", sc2: "", sc3: "" } },
         notes: ""
       }
     ]
