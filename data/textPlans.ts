db93b6335f16c658e208b29a8ebc9d2eef16fc3f
Atharva <169069337+Atharva11042k8@users.noreply.github.com>
2026-02-01 11:23:19 +0530

    Update textPlans.ts

--- data/textPlans.ts
+++ data/textPlans.ts
@@ -1,18 +1,10 @@
 export const textPlans = [
   {
     "id": "1",
-    "title": "Weekly Targets (Nov Week 1)",
-    "content": "- Complete Electrostatics PYQs (2019-2023)\n- Revise Organic GOC notes\n- 1 Full Syllabus Mock Test on Sunday",
-    "type": "Weekly",
-    "isCompleted": false,
-    "createdAt": "2023-11-01"
-  },
-  {
-    "id": "2",
-    "title": "Daily To-Do",
-    "content": "- 20 Maths questions (Conic Sections)\n- Read NCERT P-Block Group 15",
+    "title": "01.02.2026",
+    "content": "- maths: linear programming,matrix,trigo \n- chemistry: solid state, solution, thermoD \n circular motion",
     "type": "Daily",
-    "isCompleted": true,
-    "createdAt": "2023-11-02"
+    "isCompleted": false,
+    "createdAt": "2025-02-01"
   }
 ];
